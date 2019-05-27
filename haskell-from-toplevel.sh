1fb4f418c50e720eab1fd74b6e27b06d0c5e6917
Donnacha Oisín Kidney <mail@doisinkidney.com>
2019-05-27 13:58:04 +0100

    fixed haskell compile script (didn't properly recognise subdirectories)

--- haskell-from-toplevel.sh
+++ haskell-from-toplevel.sh
@@ -2,5 +2,6 @@
 withcode=$1
 nocode=${withcode#"haskell/"}
 cd haskell || exit
+dir=$(dirname "$nocode")
 file=$(basename "$nocode" .lhs).tex
-lhs2TeX -o "$file" "$nocode"
+lhs2TeX -o "$dir/$file" "$nocode"
