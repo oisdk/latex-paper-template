b0ca61e8487de4ddc204806b45eb2a7103c8b764
oisdk <mail@doisinkidney.com>
2020-07-09 23:10:13 +0100

    cleaned up scripts

--- scripts/agda-from-toplevel.sh
+++ scripts/agda-from-toplevel.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Agda needs to be run in the source directory of the project to work
-# correctly. This runs Agda on a file from within the agda directory.
+# correctly.
+# Run this script with a path (like agda/Example.lagda) from the
+# directory *above* agda/
 
 cd agda || exit
 agda --latex --latex-dir=. "${1#agda/}"
--- scripts/haskell-from-toplevel.sh
+++ scripts/haskell-from-toplevel.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 withcode=$1
 nocode=${withcode#"haskell/"}
 cd haskell || exit
